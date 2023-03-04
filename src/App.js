eecb6a631c8c97d53cc41a8667c009eb57238e32
macjonessoriano <sorianomacjones@gmail.com>
2023-03-04 18:50:04 +0800

    added some p tag in index.html

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ class App extends Component {
 		this.state = {
 			monsters: [],
 		};
-		console.log('1');
+		console.log('constructor');
 	}
 
 	componentDidMount() {
-		console.log('3');
+		console.log('componentDidMount');
 		fetch('https://jsonplaceholder.typicode.com/users')
 			.then((response) => response.json())
 			.then((users) =>
@@ -29,9 +29,17 @@ class App extends Component {
 	}
 
 	render() {
-		console.log('2');
+		console.log('Render');
 		return (
 			<div className='App'>
+				<input
+					className='search-box'
+					type='search'
+					placeholder='search-monsters'
+					onChange={(event) => {
+						console.log(event);
+					}}
+				/>
 				{this.state.monsters.map((monster) => {
 					return (
 						<div key={monster.id}>
