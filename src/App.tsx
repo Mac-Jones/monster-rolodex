1a983e384f590ad13159b2ad52b0ad2bcf7512f4
macjonessoriano <sorianomacjones@gmail.com>
2023-03-10 22:16:04 +0800

    vid203-205 typing the monster rolodex using typescript final

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,35 @@
-import { useState, useEffect } from 'react';
-
-import './App.css';
+import { useState, useEffect, ChangeEvent } from 'react';
 
 import CardList from './component/card-list/card-list.component';
 import SearchBox from './component/search-box/search-box.component';
 
+import { getData } from './utils/data.utils';
+import './App.css';
+
+export type Monster = {
+	id: string;
+	name: string;
+	email: string;
+};
+
 const App = () => {
 	const [searchField, setSearchField] = useState('');
-	const [monsters, setMonsters] = useState([]);
+	const [monsters, setMonsters] = useState<Monster[]>([]);
 	const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
-	console.log('rendered');
-
 	useEffect(() => {
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then((response) => response.json())
-			.then((users) => setMonsters(users));
+		// fetch('https://jsonplaceholder.typicode.com/users')
+		// 	.then((response) => response.json())
+		// 	.then((users) => setMonsters(users));
+
+		const fetchUsers = async () => {
+			const users = await getData<Monster[]>(
+				'https://jsonplaceholder.typicode.com/users'
+			);
+			setMonsters(users);
+		};
+
+		fetchUsers();
 	}, []);
 
 	useEffect(() => {
@@ -26,7 +40,7 @@ const App = () => {
 		setFilteredMonsters(newFilteredMonsters);
 	}, [monsters, searchField]);
 
-	const onSearchChange = (event) => {
+	const onSearchChange = (event: ChangeEvent<HTMLInputElement>): void => {
 		const searchFieldString = event.target.value.toLocaleLowerCase();
 		setSearchField(searchFieldString);
 	};
--- src/component/search-box/search-box.component.tsx
+++ src/component/search-box/search-box.component.tsx
@@ -1,24 +1,11 @@
-import { ChangeEventHandler } from 'react';
+import { ChangeEvent } from 'react';
 
 import './search-box.styles.css';
-
-// typing functions
-// const func: (a: string, b: number, c: boolean) => void = (a,b,c) => {}
-
-//typing objects
-// interface ISearchBoxProps {
-// 	className: string;
-// 	placeholder?: string;
-// }
-
-// interface ISearchBoxProps {
-// 	onChangeHandler: (a: string) => void;
-// }
 
 type SearchBoxProps = {
 	className: string;
 	placeholder?: string;
-	onChangeHandler: ChangeEventHandler<HTMLInputElement>;
+	onChangeHandler: (event: ChangeEvent<HTMLInputElement>) => void;
 };
 
 const SearchBox = ({
